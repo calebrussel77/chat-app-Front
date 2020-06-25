df752b16dc24628a678995dc399510ff158c8e01
caleb russel <calebrussel77@gmail.com>
2020-06-25 18:22:05 +0200

    setting up socket.io

---
+++ src/containers/Chat/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Chat = () => (
+    <div>
+        <p>chat</p>
+    </div>
+);
+
+export default Chat;
---
+++ src/containers/Join/Join.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Join = () => (
+    <div>
+        <p>join</p>
+        <input className =''/>
+    </div>
+);
+
+export default Join;
